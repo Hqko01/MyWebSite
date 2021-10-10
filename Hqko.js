b58305f596330e410b9a0b86921d3311fd38ddab
Hqko <74988116+Hqko01@users.noreply.github.com>
2021-10-10 23:09:47 +0300

    Add files via upload

---
+++ Hqko.js
@@ -0,0 +1,17 @@
+function copy() {
+    var copyText = "Hqko#4059"
+    navigator.clipboard.writeText(copyText)
+}
+
+function botOpen() {
+    window.open(" https://discord.com/oauth2/authorize?client_id=869536273872879626&permissions=8&scope=bot")
+}
+
+function gitOpen() {
+    window.open("https://github.com/Hqko01")
+}
+
+function atCopy() {
+    var copyATText = "hakan34yilmaz01@gmail.com"
+    navigator.clipboard.writeText(copyATText)
+}
